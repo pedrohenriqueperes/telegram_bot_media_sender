32dc21a7a564be394d214527a61ff53e1d914b6d
pedrohenriqueperes <php741@protonmail.com>
2024-04-04 16:49:35 -0300

    while loop added

---
+++ credencials.py
@@ -0,0 +1,6 @@
+media = -1002048403616
+
+
+channel_id = media
+folder = "/Users/pedroperes/Downloads/output"
+bot_api = "7128238583:AAE_mYgru1oUF02JguVLI0nNWbuYuuM5Zxc"
--- main.py
+++ main.py
@@ -1,54 +1,52 @@
 import os
 import telebot
 from time import sleep
+from credencials import *
 
-channel_id = int(input('Channel ID: '))
-folder_to_post = str(input("Provide Folder Path containing the photos: "))
-bot_api = str(input("Insert Bot Api Keys: "))
-bot = telebot.TeleBot(bot_api)  
+
+channel_id = media
+folder_to_post = folder
+bot = telebot.TeleBot(bot_api)
 
 def send_media_files(folder_path, channel_id, bot):
     """
-    Iterates through media files in a folder and sends them to a Telegram channel in alphabetical order.
-
-    Args:
-        folder_path (str): The path to the folder containing the media files.
-        channel_id (str): The Telegram channel ID to send messages to.
-        bot (TeleBot): Your initialized TeleBot object.
+    Itera pelos arquivos de mídia em uma pasta e os envia para um canal do Telegram em ordem alfabética.
     """
-
     file_list = os.listdir(folder_path)
-    file_list.sort()  # Sort the list alphabetically
+    file_list.sort()  # Ordena a lista alfabeticamente
 
     for filename in file_list:
         file_path = os.path.join(folder_path, filename)
-
-        if os.path.isfile(file_path):  # Check if it's a file (not a subdirectory)
+        if os.path.isfile(file_path):  # Verifica se é um arquivo (não um subdiretório)
             try:
                 with open(file_path, 'rb') as f:
                     if filename.lower().endswith(('.jpg', '.jpeg', '.png')):
                         bot.send_photo(channel_id, f)
-
+                        print('sent')
+                        sleep(1)  # Pausa de 1 segundo
 
                     elif filename.lower().endswith('.mp4'):
                         bot.send_video(channel_id, f)
-
+                        print('sent')
+                        sleep(1)  # Pausa de 1 segundo
 
                     elif filename.lower().endswith(('.gif', '.webp')):
                         bot.send_document(channel_id, f)
+                        print('sent')
+                        sleep(1)  # Pausa de 1 segundo
 
-
-                    os.remove(file_path)  # Delete the file after successful sending
+                    os.remove(file_path)  # Deleta o arquivo após enviar com sucesso
 
             except Exception as e:
-                print(f"Error sending {filename}: {str(e)}")
+                print(f"Erro ao enviar {filename}: {str(e)}")
 
-            sleep(10)  # Delay 10 seconds
+            #sleep(10)  # Pausa de 10 segundos entre envios
 
 if __name__ == "__main__":
     while True:
         file_list = os.listdir(folder_to_post)
         if not file_list:
-            break
+            print("Diretório vazio. Aguardando 15 segundos antes de verificar novamente.")
+            sleep(15)  # Pausa de 15 segundos antes de verificar novamente
         else:
             send_media_files(folder_to_post, channel_id, bot)
